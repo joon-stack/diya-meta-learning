14d4df6c9fe8a4f0d2eb5cdd7bf274ab1b4b2344
joon-stack <zuni1218@gmail.com>
2022-10-18 18:22:50 +0900

    Add skeletons

---
+++ hw0/models.py
@@ -0,0 +1,121 @@
+"""
+Classes defining user and item latent representations in
+factorization models.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ScaledEmbedding(nn.Embedding):
+    """
+    Embedding layer that initialises its values
+    to using a normal variable scaled by the inverse
+    of the embedding dimension.
+    """
+
+    def reset_parameters(self):
+        """
+        Initialize parameters.
+        """
+
+        self.weight.data.normal_(0, 1.0 / self.embedding_dim)
+        if self.padding_idx is not None:
+            self.weight.data[self.padding_idx].fill_(0)
+            
+    
+class ZeroEmbedding(nn.Embedding):
+    """
+    Embedding layer that initialises its values
+    to using a normal variable scaled by the inverse
+    of the embedding dimension.
+
+    Used for biases.
+    """
+
+    def reset_parameters(self):
+        """
+        Initialize parameters.
+        """
+
+        self.weight.data.zero_()
+        if self.padding_idx is not None:
+            self.weight.data[self.padding_idx].fill_(0)
+            
+        
+class MultiTaskNet(nn.Module):
+    """
+    Multitask factorization representation.
+
+    Encodes both users and items as an embedding layer; the likelihood score
+    for a user-item pair is given by the dot product of the item
+    and user latent vectors. The numerical score is predicted using a small MLP.
+
+    Parameters
+    ----------
+
+    num_users: int
+        Number of users in the model.
+    num_items: int
+        Number of items in the model.
+    embedding_dim: int, optional
+        Dimensionality of the latent representations.
+    layer_sizes: list
+        List of layer sizes to for the regression network.
+    sparse: boolean, optional
+        Use sparse gradients.
+    embedding_sharing: boolean, optional
+        Share embedding representations for both tasks.
+
+    """
+
+    def __init__(self, num_users, num_items, embedding_dim=32, layer_sizes=[96, 64], 
+                 sparse=False, embedding_sharing=True):
+
+        super().__init__()
+
+        self.embedding_dim = embedding_dim
+
+        #********************************************************
+        #******************* YOUR CODE HERE *********************
+        #********************************************************
+        
+        
+        #********************************************************
+        #******************* YOUR CODE HERE *********************
+        #********************************************************
+        
+    def forward(self, user_ids, item_ids):
+        """
+        Compute the forward pass of the representation.
+
+        Parameters
+        ----------
+
+        user_ids: tensor
+            A tensor of integer user IDs of shape (batch,)
+        item_ids: tensor
+            A tensor of integer item IDs of shape (batch,)
+
+        Returns
+        -------
+
+        predictions: tensor
+            Tensor of user-item interaction predictions of 
+            shape (batch,). This corresponds to p_ij in the 
+            assignment.
+        score: tensor
+            Tensor of user-item score predictions of shape 
+            (batch,). This corresponds to r_ij in the 
+            assignment.
+        """
+        
+        #********************************************************
+        #******************* YOUR CODE HERE *********************
+        #********************************************************
+
+
+        #********************************************************
+        #********************************************************
+        #********************************************************
+        return predictions, score
